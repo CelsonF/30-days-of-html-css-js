e48089a8b83e9d03182605b993b25dade545f57a
CelsonF <celson338540@gmail.com.com>
2021-02-19 23:40:15 -0300

    Created the second Challenge

---
+++ root/Celson/Challenge_02/assets/js/script.js
@@ -0,0 +1,45 @@
+function sliderJs() {
+const imgBx = document.querySelector('.imgBx');
+const slides = imgBx.getElementsByTagName('img');
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+let count = 0;
+
+next.addEventListener('click',function nextSlide() {
+    slides[count].classList.remove('active');
+    count = (count + 1) % slides.length;
+    slides[count].classList.add('active');
+});
+
+prev.addEventListener('click',function prevSlide() {
+    slides[count].classList.remove('active');
+    count = (count - 1 + slides.length) % slides.length;
+    slides[count].classList.add('active');
+});
+}
+
+
+
+function sliderText() {
+    const contentBX = document.querySelector('.contentBx');
+    const slidesText = contentBX.getElementsByTagName('div');
+    const prev = document.querySelector('.prev');
+    const next = document.querySelector('.next');
+    let count = 0;
+    
+    next.addEventListener('click',function nextSlideText() {
+        slidesText[count].classList.remove('active');
+        count = (count + 1) % slidesText.length;
+        slidesText[count].classList.add('active');
+    });
+    
+    prev.addEventListener('click',function prevSlideText() {
+        slidesText[count].classList.remove('active');
+        count = (count - 1 + slidesText.length) % slidesText.length;
+        slidesText[count].classList.add('active');
+    });
+}
+
+
+sliderJs();
+sliderText();
