bb1661d8c6696f2890aeaf2e565cb77b5cd8c819
CelsonF <celson338540@gmail.com.com>
2021-02-23 23:35:50 -0300

    Responsive Layout

--- root/Celson/Challenge_03/assets/js/script.js
+++ root/Celson/Challenge_03/assets/js/script.js
@@ -8,3 +8,11 @@ circle.style.background = colors;
 }
 
 
+function toggleMenu() {
+    let menuToggle = document.querySelector('.toggle');
+    let navigation = document.querySelector('.navigation');
+
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
+
