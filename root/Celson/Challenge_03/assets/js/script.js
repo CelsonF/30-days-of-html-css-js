45dfeff47362d606637b5ff1bfac2fcb18c593b4
CelsonF <celson338540@gmail.com.com>
2021-02-23 22:35:49 -0300

    Finaly Challenge 03

--- root/Celson/Challenge_03/assets/js/script.js
+++ root/Celson/Challenge_03/assets/js/script.js
@@ -2,5 +2,9 @@ function imgSlider (anything) {
     document.querySelector('.starbucks').src = anything;
 }
 
+function changeColors(colors) {
+const circle = document.querySelector('.circle');
+circle.style.background = colors;
+}
 
 
